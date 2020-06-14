6321fb0367228505fdbf9c1fcc670f07a26111a0
Scott Edwards <scotteza@gmail.com>
2020-06-14 19:22:06 +0100

    Add xUnit runner references and example test

---
+++ src/FinancialIndependencePlanner.Test/Acceptance/FinancialIndependencePlannerShould.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FinancialIndependencePlanner.Test.Acceptance
+{
+    public class FinancialIndependencePlannerShould
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 1 + 3);
+        }
+    }
+}
